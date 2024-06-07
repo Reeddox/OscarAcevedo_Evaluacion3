cc322cf4f135a2388e35fe76ba1e4a184efb440c
Reeddox <oscaracevedosanchez@gmail.com>
2024-06-07 10:20:11 -0400

    Arreglos en los js
    Se arreglaron algunos import que no tenian el .js al final

--- assets/js/promesas.js
+++ assets/js/promesas.js
@@ -1,6 +1,8 @@
-import{ collection, addDoc, doc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js"
-import { db } from '/.firebase.js';
+import{ collection, addDoc} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { db } from './firebase.js';
 
-export const registrarUsuarios = async(usuario)=>{
-    const docRef = await addDoc(collection(db, "usuario"))
+export const registrarUsuarios = async(Usuarios)=>{
+    console.log("Usuarios");
+    console.log(Usuarios);
+    const docRef = await addDoc(collection(db, "Usuarios"), Usuarios);
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-import { registrarUsuarios } from "./promesas";
+import { registrarUsuarios } from "./promesas.js";
 window.addEventListener("load", () =>{
     document.getElementById("btnEnviar").addEventListener("click", registrar);
 });
@@ -8,9 +8,9 @@ const registrar = () =>{
     let eApellido = document.getElementById("apellido");
     let eEmail = document.getElementById("email");
     let eFechaN = document.getElementById("fechaNacimiento");
-    let eSexo = document.querySelector(['input(name=sexo']);
+    let eSexo = document.querySelector('input[name=sexo]:checked');
     let eTelefono = document.getElementById("telefono");
-    let eAsunto = document.getElementById("asunto");
+    let eAsunto = document.getElementById("opciones");
     let eMensaje = document.getElementById("mensaje");
     let eArchivo = document.getElementById("archivo");
     //----------------------------------------------//
@@ -24,16 +24,12 @@ const registrar = () =>{
     let nMensaje = eMensaje.value;
     let nArchivo = eArchivo;
     //----------------------------------------------//
-    let objeto = {nombre:nNombre, apellido:nApellido, email:nEmail, fecha:nFechaN, sexo:eSexo,telefono:nTelefono, asunto:nAsunto, mensaje:nMensaje, archivo:nArchivo};
+    let objeto = {nombre:nNombre, apellido:nApellido, email:nEmail, fecha:nFechaN, sexo:nSexo,telefono:nTelefono, asunto:nAsunto, mensaje:nMensaje, archivo:nArchivo};
+    console.log(objeto)
     registrarUsuarios(objeto).then(()=>{
         alert("Registrado con Exito");
     }).catch((a)=>{
         alert("ERROR");
         alert(a);
     })
-}
-
-function cambiarColor(){
-    var element = document.body;
-    element.classlist.toggle("dark-mode");
 }
