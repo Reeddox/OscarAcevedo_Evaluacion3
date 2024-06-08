c1a4be95c4fa4d2c5577f1f777f8ac3b1bc29ae6
Reeddox <oscaracevedosanchez@gmail.com>
2024-06-08 17:44:08 -0400

    Comentarios
    Se agregaron comentarios en promesas para explicar que hace cada funcion

--- assets/js/promesas.js
+++ assets/js/promesas.js
@@ -1,12 +1,13 @@
 import{ collection, addDoc, getDocs, updateDoc, deleteDoc, doc} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import { db } from './firebase.js';
-
+//La funcion registrarUsuarios sirve para agregar un nuevo documento en la coleccion Usuarios//
 export const registrarUsuarios = async(Usuarios)=>{
     console.log("Usuarios");
     console.log(Usuarios);
     const docRef = await addDoc(collection(db, "Usuarios"), Usuarios);
 }
 
+//La funcion obtenerUsuarios recupera todos los documentos de la coleccion Usuarios//
 export const obtenerUsuarios = async() => {
     const ref = collection(db, "Usuarios");
     const querySnapshot = await getDocs(ref);
@@ -20,11 +21,13 @@ export const obtenerUsuarios = async() => {
     return listado;
 }
 
+//La funcion actualizarUsuarios actualiza un documento ya existente en la coleccion Usuarios//
 export const actualizarUsuarios = async(Usuarios,id) => {
     const ref = doc(db, "Usuarios", id);
     await updateDoc(ref, Usuarios);
 }
 
+//La funcion eliminarUsuarios elimina un documento de la coleccion Usuarios//
 export const eliminarUsuarios = async(id) => {
     const ref = doc(db, "Usuarios", id);
     await deleteDoc(ref)
