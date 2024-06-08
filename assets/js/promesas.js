4b159dad6e2ccb9f24fe0bd7286074e698442531
Reeddox <oscaracevedosanchez@gmail.com>
2024-06-08 15:11:45 -0400

    Cambios Grandes
    Se elimino el campo archivo y se agregaron los checkbox, se pudo registrar datos en la base de datos de firebase, se agrego la tabla para actualizar o eliminar datos y se hizo funcional el boton eliminar de la tabla

--- assets/js/promesas.js
+++ assets/js/promesas.js
@@ -1,8 +1,31 @@
-import{ collection, addDoc} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import{ collection, addDoc, getDocs, updateDoc, deleteDoc, doc} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import { db } from './firebase.js';
 
 export const registrarUsuarios = async(Usuarios)=>{
     console.log("Usuarios");
     console.log(Usuarios);
     const docRef = await addDoc(collection(db, "Usuarios"), Usuarios);
+}
+
+export const obtenerUsuarios = async() => {
+    const ref = collection(db, "Usuarios");
+    const querySnapshot = await getDocs(ref);
+    console.log(querySnapshot);
+    let listado = [];
+    querySnapshot.forEach(doc => {
+        console.log(doc.data());
+        listado.push({...doc.data(),id:doc.id});
+    });
+    console.log(listado);
+    return listado;
+}
+
+export const actualizarUsuarios = async(Usuarios,id) => {
+    const ref = doc(db, "Usuarios", id);
+    await updateDoc(ref, Usuarios);
+}
+
+export const eliminarUsuarios = async(id) => {
+    const ref = doc(db, "Usuarios", id);
+    await deleteDoc(ref)
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
-import { registrarUsuarios } from "./promesas.js";
+import { registrarUsuarios, obtenerUsuarios, actualizarUsuarios, eliminarUsuarios } from "./promesas.js";
 window.addEventListener("load", () =>{
     document.getElementById("btnEnviar").addEventListener("click", registrar);
+    document.getElementById("btnEnviar").addEventListener("click", actualizar);
+    cargar_datos();
 });
 
 const registrar = () =>{
@@ -10,9 +12,12 @@ const registrar = () =>{
     let eFechaN = document.getElementById("fechaNacimiento");
     let eSexo = document.querySelector('input[name=sexo]:checked');
     let eTelefono = document.getElementById("telefono");
+    let ePersonaje = [];
+    document.querySelectorAll('input[name=Personaje]:checked').forEach((checkbox) => {
+        ePersonaje.push(checkbox.value);
+    });
     let eAsunto = document.getElementById("opciones");
     let eMensaje = document.getElementById("mensaje");
-    let eArchivo = document.getElementById("archivo");
     //----------------------------------------------//
     let nNombre = eNombre.value;
     let nApellido = eApellido.value;
@@ -20,18 +25,118 @@ const registrar = () =>{
     let nFechaN = eFechaN.value;
     let nSexo = eSexo.value;
     let nTelefono = eTelefono.value;
+    //En ePersonaje no va el .value porque los array no tienen esa propiedad//
+    let nPersonaje = ePersonaje;
     let nAsunto = eAsunto.value;
     let nMensaje = eMensaje.value;
-    let nArchivo = eArchivo;
     //----------------------------------------------//
-    let objeto = {nombre:nNombre, apellido:nApellido, email:nEmail, fecha:nFechaN, sexo:nSexo,telefono:nTelefono, asunto:nAsunto, mensaje:nMensaje, archivo:nArchivo};
-    console.log(objeto)
+    let objeto = {nombre:nNombre, apellido:nApellido, email:nEmail, fecha:nFechaN, sexo:nSexo, telefono:nTelefono, personaje:nPersonaje, asunto:nAsunto, mensaje:nMensaje};
+    console.log(objeto);
     registrarUsuarios(objeto).then(()=>{
         alert("Registrado con Exito");
     }).catch((a)=>{
         alert("ERROR");
         alert(a);
     })
+}
+
+const cargar_datos = () => {
+    obtenerUsuarios().then((Usuarios) => {
+        console.log("Datos recuperados");
+        let estructura = "";
+        Usuarios.forEach((Usuarios) => {
+            estructura+="<tr>";
+            estructura+="<td>"+Usuarios.nombre+"</td>";
+            estructura+="<td>"+Usuarios.apellido+"</td>";
+            estructura+="<td>"+Usuarios.email+"</td>";
+            estructura+="<td>"+Usuarios.fecha+"</td>";
+            estructura+="<td>"+Usuarios.sexo+"</td>";
+            estructura+="<td>"+Usuarios.telefono+"</td>";
+            estructura+="<td>"+Usuarios.personaje.join(', ')+"</td>"; //Esto une el array en una cadena//
+            estructura+="<td>"+Usuarios.asunto+"</td>";
+            estructura+="<td>"+Usuarios.mensaje+"</td>";
+            estructura+="<td><button id= ACT"+Usuarios.id+"> Actualizar </button></td>";
+            estructura+="<td><button id= DEL"+Usuarios.id+"> Eliminar </button></td>";
+            estructura+="</tr>";
+        });
+
+    document.getElementById("tablaDatos").innerHTML = estructura;
+    Usuarios.forEach((Usuarios) => {
+        let btnActualizar = document.getElementById("ACT"+Usuarios.id);
+        btnActualizar.addEventListener("click", () => {
+            let eNombre = document.getElementById("nombre");
+            let eApellido = document.getElementById("apellido");
+            let eEmail = document.getElementById("email");
+            let eFechaN = document.getElementById("fechaNacimiento");
+            let eSexo = document.querySelector('input[name=sexo][value="${Usuarios.sexo}"]');
+            let eTelefono = document.getElementById("telefono");
+            let ePersonaje = document.querySelectorAll('input[name=Personaje]');
+            let eAsunto = document.getElementById("opciones");
+            let eMensaje = document.getElementById("mensaje");
+            //----------------------------------------------//
+            eNombre.value = Usuarios.nombre;
+            eApellido.value = Usuarios.apellido;
+            eEmail.value = Usuarios.email;
+            eFechaN.value = Usuarios.fecha;
+            if (eSexo) eSexo.checked = true;
+            eTelefono.value = Usuarios.telefono;
+            ePersonaje.forEach(checkbox => {
+                checkbox.checked = Usuarios.personaje.includes(checkbox.value);
+            }); //Esta linea recorre todos los checkbox y los marca si el valor esta en el array Usuario.personaje //
+            eAsunto.value = Usuarios.asunto;
+            eMensaje.value = Usuarios.mensaje;
+            document.getElementById("btnEnviar").value = Usuarios.id;
+        });
+        
+        let btnEliminar = document.getElementById("DEL"+Usuarios.id);
+        btnEliminar.addEventListener("click", () => {
+            if(confirm("¿Quieres eliminar a :\nNombre"+Usuarios.nombre+" "+Usuarios.apellido+"?")){
+                eliminarUsuarios(Usuarios.id).then(() => {
+                    alert("Eliminado");
+                    cargar_datos();
+                })
+            }
+        });
+    });
+});
+}
+
+const actualizar = () => {
+    let eNombre = document.getElementById("nombre");
+    let eApellido = document.getElementById("apellido");
+    let eEmail = document.getElementById("email");
+    let eFechaN = document.getElementById("fechaNacimiento");
+    let eSexo = document.querySelector('input[name=sexo]:checked');
+    let eTelefono = document.getElementById("telefono");
+    let ePersonaje = [];
+    document.querySelectorAll('input[name=Personaje]:checked').forEach((checkbox) => {
+        ePersonaje.push(checkbox.value);
+    });
+    let eAsunto = document.getElementById("opciones");
+    let eMensaje = document.getElementById("mensaje");
+
+    //----------------------------------------------//
+    let nNombre = eNombre.value;
+    let nApellido = eApellido.value;
+    let nEmail = eEmail.value;
+    let nFechaN = eFechaN.value;
+    let nSexo = eSexo.value;
+    let nTelefono = eTelefono.value;
+    //En ePersonaje no va el .value porque los array no tienen esa propiedad//
+    let nPersonaje = ePersonaje;
+    let nAsunto = eAsunto.value;
+    let nMensaje = eMensaje.value;
+    //----------------------------------------------//
+    let objeto = {nombre:nNombre, apellido:nApellido, email:nEmail, fecha:nFechaN, sexo:nSexo, telefono:nTelefono, personaje:nPersonaje, asunto:nAsunto, mensaje:nMensaje};
+    console.log(objeto);
+    let id = document.getElementById("btnEnviar").value;
+    actualizarUsuarios(objeto, id).then(() => {
+        alert("Usuario Actualizado");
+        cargar_datos();
+    }).catch((a) => {
+        alert("ERROR");
+        alert(a);
+    });
 }
 
 //Son las funciones capaces de cambiar el contraste y fuente de la pagina//
